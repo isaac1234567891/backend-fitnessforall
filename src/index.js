4df1591c4810f169dd4e0c1cae83fe4d20fff230
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-10 19:39:30 -0500

    [+] .gitgnore [+] index.js: Lanza servidor en puerto 3000 & crea 3 endpoints

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log( 'Bienvenidos a esta API' );
+const express = require( 'express' );
+const app = express();
+
+/** EndPoints de nuestro servidor */
+
+// http://localhost:3000/
+app.get( '/', ( req, res ) => {
+    res.send( '<h1>Home</h1>' );
+});
+
+// http://localhost:3000/about-us
+app.get( '/about-us', function( req, res ) {
+    res.send( '<h1>Sobre nosotros<h1/>' )
+});
+
+// http://localhost:3000/services
+app.get( '/services', function( req, res ) {
+    res.send( '<h1>Nuestros Servicios</h1>' )
+});
+
+
+/** Lanzamos el servidor en puerto indicado 
+ * http://localhost:3000
+*/
+app.listen( 3000, function() {
+    console.log( 'Servidor corriendo en puerto 3000' );
+});
