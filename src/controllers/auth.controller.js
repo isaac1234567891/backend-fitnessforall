21b9de7ea0eaac243ad9546dcca3f7a2a19e267d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-23 19:05:57 -0500

    [+] auth.controller.js: Controlar excepciones registro & login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,69 +7,89 @@ const { generateToken } = require( '../helpers/jwt.helper' );
 async function register( req, res ) {
     // Paso 1: Obtener los datos a registrar (usuario)
     const inputData = req.body;
-    
-    // Paso 2: Verificar si el usuario existe BD  ---> email
-    const userFound = await dbGetUserByUsername( inputData.username );
 
-    if( userFound ) {
-        return res.json({
+    try {
+        // Paso 2: Verificar si el usuario existe BD  ---> email
+        const userFound = await dbGetUserByUsername( inputData.username );
+
+        if( userFound ) {
+            return res.json({
+                ok: false,
+                msg: 'El usuario ya existe.'
+            });
+        }
+
+        // Paso 3: Registrar usuario (No existe)
+        const data =  await dbRegisterUser( inputData );
+        console.log( data );   
+        
+        // Paso Opcional: Generar las credenciales (Token) y esto autenticara al usuario
+
+        // Paso 4: Responder al cliente, si el usuario a sido registrado
+        res.json({
+            ok: true,
+            data
+        });
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.json({
             ok: false,
-            msg: 'El usuario ya existe.'
+            msg: 'Error al registrar usuario'
         });
     }
-
-    // Paso 3: Registrar usuario (No existe)
-    const data =  await dbRegisterUser( inputData );
-    console.log( data );
     
-    // Paso Opcional: Generar las credenciales (Token) y esto autenticara al usuario
-
-    // Paso 4: Responder al cliente, si el usuario a sido registrado
-    res.json({
-        ok: true,
-        data
-    });
 }
 
 async function login( req, res ) {
     // Paso 1: Obtener los datos para autenticar el usuario (username, password) 
     const inputData = req.body;
 
-    // Paso 2: Verificar si el usuario existe en la BD  -->  email
-    const userFound = await dbGetUserByUsername( inputData.username );
-     
-    if( ! userFound ) {
-        return res.json({
-            ok: false,
-            msg: 'El usuario no existe. Por favor registrese'
+    try {
+        // Paso 2: Verificar si el usuario existe en la BD  -->  email
+        const userFound = await dbGetUserByUsername( inputData.username );
+        
+        if( ! userFound ) {
+            return res.json({
+                ok: false,
+                msg: 'El usuario no existe. Por favor registrese'
+            });
+        }
+
+        // Paso 3: Verificar si la contrasenia conhincide
+        const isValidPassword = verifyEncriptedPassword( inputData.password, userFound.password );
+
+        if( ! isValidPassword ) {
+            return res.json({
+                ok: false,
+                msg: 'Contraseña invalida'
+            });
+        }
+
+        // Paso 4: Generar credencial para autenticacion pasiva (Token)
+        const payload = {
+            id: userFound._id,
+            name: userFound.name,
+            username: userFound.username,
+            role: userFound.role
+        };
+        
+        const token = generateToken( payload );
+
+        // Paso 5: Responder al cliente enviandole el Token
+        res.json({
+            ok: true,
+            token
         });
-    }
 
-    // Paso 3: Verificar si la contrasenia conhincide
-    const isValidPassword = verifyEncriptedPassword( inputData.password, userFound.password );
-
-    if( ! isValidPassword ) {
-        return res.json({
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.json({
             ok: false,
-            msg: 'Contraseña invalida'
-        });
+            msg: 'Error al ingresar a la aplicacion'
+        });        
     }
-
-    // Paso 4: Generar credencial para autenticacion pasiva (Token)
-    const payload = {
-        id: userFound._id,
-        name: userFound.name,
-        username: userFound.username,
-        role: userFound.role
-    };
-    
-    const token = generateToken( payload );
-
-    // Paso 5: Responder al cliente enviandole el Token
-    res.json({
-        ok: true,
-        token
-    });
 }
 
 function reNewToken( req, res ) {
@@ -78,7 +98,7 @@ function reNewToken( req, res ) {
         msg: 'Renueva las credenciales (Token)'
     });
 }
-
+   
 
 module.exports = {
     register,
