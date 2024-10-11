d172883fab0f96c805ade35a2751b148bab54dcb
yolbert isaac rivas camacho <yolbertrivas804@gmail.com>
2024-10-10 21:25:12 -0500

    [+] se realizo el delete del password de los usuarios que devuelve al frontend

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,14 +6,20 @@ const dbGetUserByUsername = async ( email ) => {
 }
 
 const dbRegisterUser = async ( newUser ) => {
-    const dbUser = new UserModel( newUser );  // Prepara los datos en JSON para registrar en MongoDB 
+    const dbUser = new UserModel( newUser);  // Prepara los datos en JSON para registrar en MongoDB 
 
     const hashPassword = encryptedPassword( dbUser.password );
     // console.log( hashPassword );
 
     dbUser.password = hashPassword;     // Reescribiendo el password original por el encriptado
 
-    return await dbUser.save();   // Guarda en la base de datos y devuelve el usuario registrado
+    const dbTempUser = await dbUser.save();   // Guarda en la base de datos y devuelve el usuario registrado
+    const dataUser = dbTempUser.toObject();
+    delete dataUser.password;
+    delete dataUser.createdAt;
+    delete dataUser.updatedAt;
+    delete dataUser.__v;
+    return dataUser;
 }
 
 
